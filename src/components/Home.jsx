63a4b60b439ea4e1054c9e5c5c4377bd23d7a000
Your Name <you@example.com>
2024-04-06 19:42:11 +0600

    set-loading

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AppContext } from "../context/App_context";
 import { FaBookmark } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
@@ -9,7 +9,16 @@ import { CgProfile } from "react-icons/cg";
 const Home = () => {
   const navigate = useNavigate();
   const [search, setSearch] = useState("");
-  const { recipe, saveFoodById, userFoodItem } = useContext(AppContext);
+  const [loading, setLoading] = useState(true); // State to track loading status
+  const { recipe, saveFoodById } = useContext(AppContext);
+
+  useEffect(() => {
+    // Simulate loading time
+    setTimeout(() => {
+      setLoading(false); // Set loading to false after some time
+    }, 2000); // Adjust this time according to your actual fetching time
+  }, []); // Empty dependency array to run this effect only once
+
   const saved = async (id) => {
     const result = await saveFoodById(id);
     toast.success(result.data.message, {
@@ -27,10 +36,7 @@ const Home = () => {
 
   // Reverse the recipe array to show the latest item first
   const reversedRecipe = [...recipe].reverse();
-  console.log(recipe);
-  console.log("This is from home", reversedRecipe);
-  console.log("This is user info form home", userFoodItem);
-  // console.log("This is user info", userFoodItem);
+
   return (
     <div className="py-3 px-5 w-full">
       <ToastContainer />
@@ -43,48 +49,54 @@ const Home = () => {
         />
       </div>
 
-      <div className="mt-4 grid grid-cols-1 md:grid-cols-4 gap-4">
-        {reversedRecipe
-          .filter((item) => {
-            return search.toLowerCase() === ""
-              ? item
-              : item.title.toLowerCase().includes(search.toLowerCase());
-          })
-          .map((item) => (
-            <div key={item?._id} className="flex items-center justify-center">
-              <div className="border-solid border-[1px] border-slate-400 shadow-md shadow-black p-2 w-full rounded-md">
-                <p className=" py-1 px-2 font-semibold flex items-center justify-start gap-2">
-                  <CgProfile className=" text-xl text-pink-600 font-semibold" />
-                  Posted by {item.user.name}
-                </p>{" "}
-                {/* Display the user's name */}
-                <div className="flex items-center justify-center">
-                  <div className="mt-2 p-2  w-fit">
-                    <img
-                      src={item.imageurl}
-                      alt="photo"
-                      className=" w-80 md:w-96  h-72 md:h-60 rounded-md"
-                    />
+      {loading ? ( // Conditionally render loading message
+        <div className=" flex items-center justify-center mt-16">
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <div className="mt-4 grid grid-cols-1 md:grid-cols-4 gap-4">
+          {reversedRecipe
+            .filter((item) => {
+              return search.toLowerCase() === ""
+                ? item
+                : item.title.toLowerCase().includes(search.toLowerCase());
+            })
+            .map((item) => (
+              <div key={item?._id} className="flex items-center justify-center">
+                <div className="border-solid border-[1px] border-slate-400 shadow-md shadow-black p-2 w-full rounded-md">
+                  <p className=" py-1 px-2 font-semibold flex items-center justify-start gap-2">
+                    <CgProfile className=" text-xl text-pink-600 font-semibold" />
+                    Posted by {item.user.name}
+                  </p>{" "}
+                  {/* Display the user's name */}
+                  <div className="flex items-center justify-center">
+                    <div className="mt-2 p-2  w-fit">
+                      <img
+                        src={item.imageurl}
+                        alt="photo"
+                        className=" w-80 md:w-96  h-72 md:h-60 rounded-md"
+                      />
+                    </div>
+                  </div>
+                  <p className=" text-left py-2 px-3 text-2xl font-semibold text-gray-800">
+                    {item.title}
+                  </p>
+                  <div className="flex items-center justify-between mt-1 mb-2 ml-2">
+                    <button
+                      onClick={() => navigate(`/${item._id}`)}
+                      className="py-1 px-3 rounded-md bg-slate-900 text-white"
+                    >
+                      View more
+                    </button>
+                    <button onClick={() => saved(item._id)}>
+                      <FaBookmark className="text-2xl" />
+                    </button>
                   </div>
                 </div>
-                <p className=" text-left py-2 px-3 text-2xl font-semibold text-gray-800">
-                  {item.title}
-                </p>
-                <div className="flex items-center justify-between mt-1 mb-2 ml-2">
-                  <button
-                    onClick={() => navigate(`/${item._id}`)}
-                    className="py-1 px-3 rounded-md bg-slate-900 text-white"
-                  >
-                    View more
-                  </button>
-                  <button onClick={() => saved(item._id)}>
-                    <FaBookmark className="text-2xl" />
-                  </button>
-                </div>
               </div>
-            </div>
-          ))}
-      </div>
+            ))}
+        </div>
+      )}
     </div>
   );
 };
